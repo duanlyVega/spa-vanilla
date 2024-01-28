565120c8fc78bf22aa82d263c70ff6f08779ae35
duanly <156867030+duanlyVega@users.noreply.github.com>
2024-01-28 23:16:48 +0000

    commit spa swa

--- app/index.js
+++ app/index.js
@@ -2,7 +2,19 @@ import api from "./helpers/wp_api.js";
 import { App } from "./App.js";
 import { d, w } from "./helpers/utility.js";
 
-d.addEventListener("DOMContentLoaded", App);
+function serviceWorker() {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("./sw.js")
+      .then((res) => console.log("Register services SW  ", res))
+      .catch((err) => console.log("Error al tratar de registar el sw", err));
+  }
+}
+
+d.addEventListener("DOMContentLoaded", (e) => {
+  serviceWorker();
+  App();
+});
 w.addEventListener("hashchange", () => {
   api.page = 1;
   App();
---
+++ sw.js
@@ -0,0 +1,70 @@
+const CACHE_NAME = "v1_cache_spa_vanilla",
+  urlsToCache = [
+    "./",
+    "./app/assets/style.css",
+    "./app/index.js",
+    "./app/assets/ball-triangle.svg",
+
+    "./app/assets/javascript.png",
+    "./app/assets/ios/16.png",
+    "./app/assets/ios/20.png",
+    "./app/assets/ios/29.png",
+    "./app/assets/ios/32.png",
+    "./app/assets/ios/40.png",
+    "./app/assets/ios/50.png",
+
+    "./app/assets/android/android-launchericon-48-48-png",
+    "./app/assets/android/android-launchericon-72-72-png",
+    "./app/assets/android/android-launchericon-96-96-png",
+    "./app/assets/android/android-launchericon-144-144-png",
+    "./app/assets/android/android-launchericon-192-192-png",
+    "./app/assets/android/android-launchericon-512-512-png",
+  ];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache).then(() => self.skipWaiting());
+      })
+      .catch((err) => console.log("Falló registro de cache", err))
+  );
+});
+
+//una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
+self.addEventListener("activate", (e) => {
+  const cacheWhitelist = [CACHE_NAME];
+
+  e.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            //Eliminamos lo que ya no se necesita en cache
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+      // Le indica al SW activar el cache actual
+      .then(() => self.clients.claim())
+  );
+});
+
+//cuando el navegador recupera una url
+self.addEventListener("fetch", (e) => {
+  //Responder ya sea con el objeto en caché o continuar y buscar la url real
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      if (res) {
+        //recuperar del cache
+        return res;
+      }
+      //recuperar de la petición a la url
+      return fetch(e.request);
+    })
+  );
+});
