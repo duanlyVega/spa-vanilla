ea82cb7625b6b6b41de8517af046aeac335e50b5
duanly <156867030+duanlyVega@users.noreply.github.com>
2024-02-01 05:01:54 +0000

    sw

--- sw.js
+++ sw.js
@@ -1,16 +1,16 @@
 const CACHE_NAME = "v1_cache_spa_vanilla",
   urlsToCache = [
     "./",
-    "/app/assets/style.css",
-    "/app/index.js",
-    "/app/assets/ball-triangle.svg",
-    "/app/assets/javascript.png",
-    "/app/assets/android/android-launchericon-48-48-png",
-    "/app/assets/android/android-launchericon-72-72-png",
-    "/app/assets/android/android-launchericon-96-96-png",
-    "/app/assets/android/android-launchericon-144-144-png",
-    "/app/assets/android/android-launchericon-192-192-png",
-    "/app/assets/android/android-launchericon-512-512-png",
+    "./app/assets/style.css",
+    "./app/index.js",
+    "./app/assets/ball-triangle.svg",
+    "./app/assets/javascript.png",
+    "./app/assets/android/android-launchericon-48-48-png",
+    "./app/assets/android/android-launchericon-72-72-png",
+    "./app/assets/android/android-launchericon-96-96-png",
+    "./app/assets/android/android-launchericon-144-144-png",
+    "./app/assets/android/android-launchericon-192-192-png",
+    "./app/assets/android/android-launchericon-512-512-png",
   ];
 
 self.addEventListener("install", (e) => {
