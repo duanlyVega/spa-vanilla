d2f9cfca77fdb37aa3dc7fccbdb937938331c15f
duanly <156867030+duanlyVega@users.noreply.github.com>
2024-02-09 15:49:53 +0000

    if

--- app/index.js
+++ app/index.js
@@ -2,14 +2,18 @@ import api from "./helpers/wp_api.js";
 import { App } from "./App.js";
 import { d, w } from "./helpers/utility.js";
 
-function serviceWorker() {
+async function serviceWorker() {
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker
       .register("./sw.js")
       .then((res) => console.log("Register services SW  ", res))
       .catch((err) => console.log("Error al tratar de registar el sw", err));
   }
+  if (!('PushManager' in window)) {
+    throw new Error('No Push API Support!')
+  }
 }
+
 serviceWorker();
 
 d.addEventListener("DOMContentLoaded", (e) => {
