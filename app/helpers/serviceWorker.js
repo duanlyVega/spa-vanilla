020d7458a898a93c08457440244b2ae36f0ff45f
duanly <156867030+duanlyVega@users.noreply.github.com>
2024-02-10 10:40:09 +0000

    poif

--- app/helpers/serviceWorker.js
+++ app/helpers/serviceWorker.js
@@ -2,17 +2,8 @@ export function serviceWorker() {
     if ("serviceWorker" in navigator) {
       navigator.serviceWorker
         .register("./sw.js")
-        .then((res) => {
-        
-          console.log("Register services SW  ", res)
-          res.installing.onstatechange=(e)=>{
-          console.log(res.installing.state)
-          console.log(e)
+        .then((res) => console.log("Register services SW  ", res))
 
-          //console.log("Register services SW  ", res)
-
-          }
-        })
         .catch((err) => console.log("Error al tratar de registrar el sw", err));
     }
 
